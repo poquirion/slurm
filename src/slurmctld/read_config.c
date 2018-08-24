92b441ed9a0c21b9960dc89fbf0bc7285ffd6059
Brian Christiansen <brian@schedmd.com>
2018-08-24 12:44:34 -0600

    1: Fix removing job licenses on reconfig/restart
    when configured license counts are 0.
    
    Bug 5630

--- src/slurmctld/read_config.c
+++ src/slurmctld/read_config.c
@@ -2547,7 +2547,7 @@ static int _restore_job_dependencies(void)
 			}
 		}
 
-		license_list = license_validate(job_ptr->licenses,
+		license_list = license_validate(job_ptr->licenses, false,
 						job_ptr->tres_req_cnt, &valid);
 		FREE_NULL_LIST(job_ptr->license_list);
 		if (valid)
